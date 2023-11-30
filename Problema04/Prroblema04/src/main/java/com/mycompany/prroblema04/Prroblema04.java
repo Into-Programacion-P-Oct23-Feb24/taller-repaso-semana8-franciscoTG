93f684e969d699555c1a5890f328f94fe6385c23
Francisco Torres <ft110565752@gmail.com>
2023-11-30 13:49:25 -0500

    Problemas

---
+++ Problema04/Prroblema04/src/main/java/com/mycompany/prroblema04/Prroblema04.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.prroblema04;
+
+/**
+ *
+ * @author ft110
+ */
+public class Prroblema04 {
+
+    public static void main(String[] args) {
+        int n = 2;
+        while (n <= 3) {
+            n = n + 1;
+        }
+        while (n <= 4) {
+            n = n + 1;
+        }
+        System.out.println(n);
+    }
+}
